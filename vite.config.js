4505de39324e9f056ae6b9807893d88d0d588c2a
Miko <gamess19394@gmail.com>
2025-07-18 17:03:41 +0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: './src',
+  server: {
+    posr: 3000,
+    opem:true,
+  },
+  build: {
+    outDir:"../build",
+  }
+})
